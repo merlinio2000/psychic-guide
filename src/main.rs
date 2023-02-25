73ad24293ec5830f52c1a2eb67e040e59afbf930
= <merlin@merlinio.ch>
2023-02-26 00:46:45 +0100

    minimize example & use log file

--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,87 @@
-//! Discover Bluetooth devices and list them.
+use bluer::{Adapter, AdapterEvent, Address, Device};
+use futures::{pin_mut, StreamExt};
+use std::{fs, io::Write};
+use tokio::sync::Mutex;
 
-use bluer::{Adapter, AdapterEvent, Address, DeviceEvent};
-use futures::{pin_mut, stream::SelectAll, StreamExt};
-use std::{collections::HashSet, env};
-
-async fn query_device(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
-    let device = adapter.device(addr)?;
-    println!("    Address type:       {}", device.address_type().await?);
-    println!("    Name:               {:?}", device.name().await?);
-    println!("    Icon:               {:?}", device.icon().await?);
-    println!("    Class:              {:?}", device.class().await?);
-    println!("    UUIDs:              {:?}", device.uuids().await?.unwrap_or_default());
-    println!("    Paired:             {:?}", device.is_paired().await?);
-    println!("    Connected:          {:?}", device.is_connected().await?);
-    println!("    Trusted:            {:?}", device.is_trusted().await?);
-    println!("    Modalias:           {:?}", device.modalias().await?);
-    println!("    RSSI:               {:?}", device.rssi().await?);
-    println!("    TX power:           {:?}", device.tx_power().await?);
-    println!("    Manufacturer data:  {:?}", device.manufacturer_data().await?);
-    println!("    Service data:       {:?}", device.service_data().await?);
-    Ok(())
+#[derive(Debug)]
+struct KnownDevice {
+    addr: Address,
+    name: Option<String>,
+    class: Option<u32>,
+    rssi_hist: Vec<Option<i16>>,
 }
 
-async fn query_all_device_properties(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
-    let device = adapter.device(addr)?;
-    let props = device.all_properties().await?;
-    for prop in props {
-        println!("    {:?}", &prop);
+impl KnownDevice {
+    async fn from(device: Device) -> bluer::Result<Self> {
+        Ok(Self {
+            addr: device.address(),
+            name: device.name().await?,
+            class: device.class().await?,
+            rssi_hist: vec![device.rssi().await?],
+        })
     }
+}
+
+async fn handle_discovery(
+    adapter: &Adapter,
+    known_devices: &Mutex<Vec<KnownDevice>>,
+    addr: Address,
+) -> bluer::Result<()> {
+    let device = adapter.device(addr)?;
+    let res = KnownDevice::from(device).await;
+
+    match res {
+        Err(err) => println!("    Error: {}", &err),
+        Ok(discovered_device) => {
+            let device_store = &mut *known_devices.lock().await;
+
+            if let Some(existing_device) = device_store.iter_mut().find(|d| d.addr == addr) {
+                existing_device
+                    .rssi_hist
+                    .push(discovered_device.rssi_hist[0]);
+            } else {
+                device_store.push(discovered_device);
+            }
+
+            let mut new_log =
+                fs::File::create("log.tmp").expect("failed to create temporary log file");
+            write!(&mut new_log, "{:#?}", device_store).expect("failed to write new_log");
+            new_log.sync_all()?;
+
+            match fs::remove_file("log.txt") {
+                Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
+                Err(err) => return Err(err.into()),
+                Ok(_) => {}
+            }
+        }
+    }
+
     Ok(())
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> bluer::Result<()> {
-    let with_changes = env::args().any(|arg| arg == "--changes");
-    let all_properties = env::args().any(|arg| arg == "--all-properties");
-    let filter_addr: HashSet<_> = env::args().filter_map(|arg| arg.parse::<Address>().ok()).collect();
-
     env_logger::init();
     let session = bluer::Session::new().await?;
     let adapter = session.default_adapter().await?;
-    println!("Discovering devices using Bluetooth adapter {}\n", adapter.name());
+    println!(
+        "Discovering devices using default Bluetooth adapter {}\n",
+        adapter.name()
+    );
     adapter.set_powered(true).await?;
+
+    let known_devices: Mutex<Vec<KnownDevice>> = Default::default();
 
     let device_events = adapter.discover_devices().await?;
     pin_mut!(device_events);
-
-    let mut all_change_events = SelectAll::new();
 
     loop {
         tokio::select! {
             Some(device_event) = device_events.next() => {
                 match device_event {
                     AdapterEvent::DeviceAdded(addr) => {
-                        if !filter_addr.is_empty() && !filter_addr.contains(&addr) {
-                            continue;
-                        }
-
                         println!("Device added: {addr}");
-                        let res = if all_properties {
-                            query_all_device_properties(&adapter, addr).await
-                        } else {
-                            query_device(&adapter, addr).await
-                        };
-                        if let Err(err) = res {
-                            println!("    Error: {}", &err);
-                        }
-
-                        if with_changes {
-                            let device = adapter.device(addr)?;
-                            let change_events = device.events().await?.map(move |evt| (addr, evt));
-                            all_change_events.push(change_events);
-                        }
+                        handle_discovery(&adapter, &known_devices, addr).await?;
                     }
                     AdapterEvent::DeviceRemoved(addr) => {
                         println!("Device removed: {addr}");
@@ -79,10 +89,6 @@ async fn main() -> bluer::Result<()> {
                     _ => (),
                 }
                 println!();
-            }
-            Some((addr, DeviceEvent::PropertyChanged(property))) = all_change_events.next() => {
-                println!("Device changed: {addr}");
-                println!("    {property:?}");
             }
             else => break
         }
