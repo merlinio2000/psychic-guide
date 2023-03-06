91a43a26c51a8779299e5b3959298ebd07844546
= <merlin@merlinio.ch>
2023-03-06 18:02:34 +0100

    move from bluetooth to wifi

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,25 @@
-use bluer::{Adapter, AdapterEvent, Address, Device};
-use futures::{pin_mut, StreamExt};
-use std::{fs, io::Write};
-use tokio::sync::Mutex;
+use pcap::Device;
 
-#[derive(Debug)]
-struct KnownDevice {
-    addr: Address,
-    name: Option<String>,
-    class: Option<u32>,
-    rssi_hist: Vec<Option<i16>>,
-}
+const DLT_IEEE802_11_RADIO: i32 = 127;
 
-impl KnownDevice {
-    async fn from(device: Device) -> bluer::Result<Self> {
-        Ok(Self {
-            addr: device.address(),
-            name: device.name().await?,
-            class: device.class().await?,
-            rssi_hist: vec![device.rssi().await?],
-        })
-    }
-}
+fn main() -> Result<(), pcap::Error> {
+    let mut cap = Device::lookup()?.unwrap().open()?;
 
-async fn handle_discovery(
-    adapter: &Adapter,
-    known_devices: &Mutex<Vec<KnownDevice>>,
-    addr: Address,
-) -> bluer::Result<()> {
-    let device = adapter.device(addr)?;
-    let res = KnownDevice::from(device).await;
+    cap.set_datalink(pcap::Linktype(DLT_IEEE802_11_RADIO))?;
 
-    match res {
-        Err(err) => println!("    Error: {}", &err),
-        Ok(discovered_device) => {
-            let device_store = &mut *known_devices.lock().await;
+    while let Ok(packet) = cap.next_packet() {
+        println!("received packet {:?}", packet);
 
-            if let Some(existing_device) = device_store.iter_mut().find(|d| d.addr == addr) {
-                existing_device
-                    .rssi_hist
-                    .push(discovered_device.rssi_hist[0]);
-            } else {
-                device_store.push(discovered_device);
+        if let Ok(radiotap_header) = radiotap::Radiotap::from_bytes(packet.data) {
+            println!("radiotap {radiotap_header:?}");
+
+            // strip away the radiotap data from the 802.11 frame
+            let payload = &packet.data[radiotap_header.header.length..];
+
+            match libwifi::parse_frame(payload) {
+                Ok(frame) => println!("802.11 frame {frame:?}"),
+                Err(err) => eprintln!("failed to parse 802.11 frame {err}"),
             }
-
-            let mut new_log =
-                fs::File::create("log.tmp").expect("failed to create temporary log file");
-            write!(&mut new_log, "{:#?}", device_store).expect("failed to write new_log");
-            new_log.sync_all()?;
-
-            match fs::remove_file("log.txt") {
-                Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
-                Err(err) => return Err(err.into()),
-                Ok(_) => {}
-            }
-        }
-    }
-
-    Ok(())
-}
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> bluer::Result<()> {
-    env_logger::init();
-    let session = bluer::Session::new().await?;
-    let adapter = session.default_adapter().await?;
-    println!(
-        "Discovering devices using default Bluetooth adapter {}\n",
-        adapter.name()
-    );
-    adapter.set_powered(true).await?;
-
-    let known_devices: Mutex<Vec<KnownDevice>> = Default::default();
-
-    let device_events = adapter.discover_devices().await?;
-    pin_mut!(device_events);
-
-    loop {
-        tokio::select! {
-            Some(device_event) = device_events.next() => {
-                match device_event {
-                    AdapterEvent::DeviceAdded(addr) => {
-                        println!("Device added: {addr}");
-                        handle_discovery(&adapter, &known_devices, addr).await?;
-                    }
-                    AdapterEvent::DeviceRemoved(addr) => {
-                        println!("Device removed: {addr}");
-                    }
-                    _ => (),
-                }
-                println!();
-            }
-            else => break
         }
     }
 
